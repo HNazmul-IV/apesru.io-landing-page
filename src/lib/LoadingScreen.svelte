4e1d2a5a1e903c71fb951e8cff2f7796e8554960
naz <nazmul.w3@gmail.com>
2022-12-24 01:31:35 +0000

    before changing header animation

--- src/lib/LoadingScreen.svelte
+++ src/lib/LoadingScreen.svelte
@@ -14,13 +14,13 @@
 
     function animate() {
         let tl = gsap.timeline({ defaults: { duration: 0.5 } });
-        tl.to(".loading-screen__gif > img", { x: -260, opacity: 0, duration: 0.3, ease: "power3.out" })
-            .to(".loading-text", { y: 100, opacity: 0, onComplete: () => (select<HTMLParagraphElement>(".loading-text").innerText = "Loading Finished.") }, "<")
-            .to(".loading-text", { y: 0, opacity: 1 }, 1)
-            .to(".loading-text", { y: `-${window.innerHeight}px`, opacity: 0, duration: 1 }, 3)
-            .to(".progress-bar", { y: `400`, duration: 1, opacity: 0 }, "<")
-            .to(".loading-screen", { y: `-${window.innerHeight}`, duration: 0.3, opacity: 0, pointerEvents: "none" })
-            .fromTo("main", { opacity: 0, duration: 0.7 }, { opacity: 1, onComplete: () => (progressFinish = true) }, "<");
+        tl; //.to(".loading-screen__gif > img", { x: -260, opacity: 0, duration: 0.3, ease: "power3.out" })
+        //     //     .to(".loading-text", { y: 100, opacity: 0, onComplete: () => (select<HTMLParagraphElement>(".loading-text").innerText = "Loading Finished.") }, "<")
+        //     //     .to(".loading-text", { y: 0, opacity: 1 }, 1)
+        //     .to(".loading-text", { y: `-${window.innerHeight}px`, opacity: 0, duration: 1 }, "<")
+        //     .to(".progress-bar", { y: `400`, duration: 1, opacity: 0 }, "<")
+        // .to(".loading-screen", { y: `-${window.innerHeight}`, duration: 1, opacity: 0, pointerEvents: "none" })
+        // .fromTo("main", { opacity: 0, duration: 1 }, { opacity: 1, onComplete: () => (progressFinish = true) }, "<");
     }
 
     function handleProgress() {
@@ -42,6 +42,7 @@
         //updating window loaded state
         window.addEventListener("load", () => (isWindowLoaded = true));
         handleProgress();
+        animate();
     });
 </script>
 
